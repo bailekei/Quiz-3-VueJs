68bf9834d9cdde779c15492cb011a750448e58b0
bailekei <bailekei@mail.gvsu.edu>
2022-04-21 05:44:26 -0400

    Changed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <h3>Keilaní Bailey</h3>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
